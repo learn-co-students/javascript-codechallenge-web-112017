14b6e8de1837099c074b6bfae859bbdcf85ce472
trewmin <trumanlahr@gmail.com>
2018-01-19 10:52:28 -0500

    done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,12 @@
 class Image {
-  
+
+  constructor({id, url, name, like_count, comments, image_id}){
+    this.id = id
+    this.url = url
+    this.name = name
+    this.like_count = like_count
+    this.comments = comments
+    this.image_id = image_id
+  }
 
 }
