bdb3add02d8890227296ccfce110b2222071ef87
iamm28 <alisonmackay246@gmail.com>
2018-01-19 10:52:15 -0500

    practice round

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,11 @@
 class Comment {
-  
+  constructor(text) {
+    this.content = text
+  }
+  render(){
+    let commentList = document.getElementById("comments")
+    let commentSpot = document.createElement("li")
+    commentSpot.innerText = this.content
+    return commentList.appendChild(commentSpot)
+  }
 }
