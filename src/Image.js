ff8cc01ef13b6ce42462a3e8288d40cf1f76530c
jerrybennett <jerrysbusiness@gmail.com>
2018-01-19 10:52:16 -0500

    first commit

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,10 @@
+let likes = 0
 class Image {
-  
-
+  constructor(obj) {
+    this.id = obj.id;
+    this.url = obj.url;
+    this.name = obj.name;
+    this.like_count = obj.like_count;
+    this.comments = obj.comments;
+  }
 }
