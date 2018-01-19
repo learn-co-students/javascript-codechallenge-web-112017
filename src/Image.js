07e842b971148cc4ee448904dc3fcf6e7665846b
github username <github email address>
2018-01-19 10:52:10 -0500

    Not quite done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,32 @@
 class Image {
-  
+
+  constructor(data){
+    this.id = data.id
+    this.url = data.url
+    this.name = data.name
+    this.likeCount = data.like_count
+    Image.all.push(this)
+  }
+
+  static increaseLikes(){
+    let numOfLikes = document.getElementById("likes")
+    console.log(numOfLikes)
+  }
+
+  render(){
+    let imgTag = document.getElementById("image")
+    imgTag.src = `${this.url}`
+
+    let name = document.getElementById("name")
+    name.innerHTML = `${this.name}`
+  }
+
+  update(){
+    let likes = this.likeCount.parseInt()
+    likes++
+    document.getElementById("likes").innerHTML = `${likes}`
+  }
 
 }
+
+Image.all = []
---
+++ src/event_listener.js
@@ -0,0 +1,29 @@
+class EventListener {
+
+  static listen(){
+    EventListener.listenForAddComment()
+    EventListener.listenForLikes()
+  }
+
+
+  static listenForAddComment(){
+    let commentForm = document.getElementById("comment_form")
+
+    commentForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      let commentInput = document.getElementById("comment_input")
+      Fetch.addComment(commentInput.value)
+    })
+  }
+
+
+  static listenForLikes(){
+    let likeButton = document.getElementById("like_button");
+
+    likeButton.addEventListener("click", event => {
+      event.preventDefault()
+      Fetch.addLike()
+    })
+  }
+}
